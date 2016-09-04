d985564c0a45ab9873f420d69005434aeb6c5119
tutzy <tutzy1@gmail.com>
2016-09-04 18:32:12 +0300

    the program is ready to use

--- UI.py
+++ UI.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -14,6 +13,10 @@ from kivy.uix.textinput import TextInput
 from kivy.properties import ListProperty, NumericProperty, ObjectProperty, StringProperty
 from kivy.base import runTouchApp
 from kivy.garden.scrolllabel import ScrollLabel
+from kivy.garden.xpopup.file import XFileOpen, XFileSave, XFolder
+from kivy.garden.xpopup.form import XSlider, XTextInput, XNotes, XAuthorization
+from kivy.garden.xpopup.notification import XNotification, XConfirmation, XError, XMessage, XProgress
+from os.path import expanduser
 from Ranker import *
 from Index_envierment import *
 from Document import *
@@ -29,6 +32,10 @@ class Menu(FloatLayout):
     Ranker_Environment = RankerEnvironment(Index)
     Doc_Count = StringProperty()
     Queries_Count = StringProperty()
+    pathToSave = ""
+    query1 = ""
+    limit = 0
+    save_flag = 0
     #history = ['MainScreen']
     #IndexDict = {} #Dictionary of the indexes
     #cur_index_selection = ''
@@ -48,16 +55,31 @@ class Menu(FloatLayout):
         self.cur_index_selection = select
         return
 """
-    def New_Index(self, path):
-        self.Index.addIndex(path)
-        print self.Index.count()
-        self.popup_msg("The index has \nbeen updated \nsuccessfully!")
+    def Update_Index(self):
+        XFileOpen(on_dismiss=self.loadIndex_filepopup_callback, path=expanduser(u'~'),multiselect=False)
         return
 
-    def Load_Queries(self,path):
-        self.Ranker_Environment.loadQueries(path)
-        self.popup_msg("The queries have \nbeen loaded \nsuccessfully!")
+    def loadIndex_filepopup_callback(self, instance):
+        if instance.is_canceled():
+            return
+        self.Index.addIndex(instance.selection[0])
+        s = 'The index was updated'
+        s += ('\nSelection: %s' % instance.selection[0])
+        XNotification(title='Pressed button: ' + instance.button_pressed + '  (will disappear after 5 seconds...)',
+                      text=s, show_time=5)
+
+    def Load_Queries(self):
+        XFileOpen(on_dismiss=self.loadQuesries_filepopup_callback, path=expanduser(u'~'), multiselect=False)
         return
+
+    def loadQuesries_filepopup_callback(self, instance):
+        if instance.is_canceled():
+            return
+        self.Ranker_Environment.loadQueries(instance.selection[0])
+        s = 'The queries was uploaded'
+        s += ('\nSelection: %s' % instance.selection[0])
+        XNotification(title='Pressed button: ' + instance.button_pressed + '  (will disappear after 5 seconds...)',
+                      text=s, show_time=5)
 
     def popup_msg(self, msg, fsize = 40):
         button_layout = AnchorLayout(anchor_x='center', anchor_y='bottom')
@@ -83,15 +105,52 @@ class Menu(FloatLayout):
         button.bind(on_press=popup.dismiss)
         popup.open()
 
-    def Run_Queries_File(self, path, name, limit):
-        self.Ranker_Environment.runQueries(pathnameToSave= path + "\\" + name + ".txt", limit=int(limit))
-        self.popup_msg("The results have \nbeen saved \nsuccessfully!")
+    def Get_Limit(self):
+        XTextInput(title='How many documents would you like \nto appear in the result list? \n(insert 0 to unlimit)', text='0', on_dismiss=self.limit_callback)
         return
 
-    def Run_Query_File(self, query, path, name, limit):
-        self.Ranker_Environment.runQuery(query=str(query), pathnameToSave=path + "\\" + name + ".txt", limit=int(limit))
-        self.popup_msg("The results have \nbeen saved \nsuccessfully!")
+
+    def limit_callback(self, instance):
+        if not instance.values['text']:
+            self.limit = 0
+        else:
+            self.limit = int(instance.values['text'])
         return
+
+
+
+    def Run_Queries_File(self, limit):
+        self.limit = int(limit)
+        XFileSave(on_dismiss=self._filepopup_Queries_callback, path=expanduser(u'~'))
+        return
+
+    def _filepopup_Queries_callback(self, instance):
+        if instance.is_canceled():
+            return
+        self.Ranker_Environment.runQueries(pathnameToSave=instance.get_full_name() + ".txt", limit=self.limit)
+        s = 'The file was saved!'
+        s += ('\nFilename: %s\nFull path: %s' % (instance.filename, instance.get_full_name()))
+        XNotification(title='Pressed button: ' + instance.button_pressed + '(will disappear after 5 seconds...)',
+                      text=s, show_time=5)
+
+    def Run_Query_File(self, query, limit):
+        self.query1 = query
+        self.limit = int(limit)
+        XFileSave(on_dismiss=self._filepopup_Query_callback, path=expanduser(u'~'))
+        return
+
+    def _filepopup_Query_callback(self, instance):
+        if instance.is_canceled():
+            return
+        self.Ranker_Environment.runQuery(query=self.query1, pathnameToSave=instance.get_full_name() + ".txt",limit=self.limit)
+        s = 'The file was saved!'
+        if instance.__class__.__name__ == 'XFileSave':
+            s += ('\nFilename: %s\nFull path: %s' %
+                  (instance.filename, instance.get_full_name()))
+        else:
+            s += ('\nSelection: %s' % instance.selection)
+        XNotification(title='Pressed button: ' + instance.button_pressed + '(will disappear after 5 seconds...)',
+                      text=s, show_time=5)
 
     def PrintDocsIDs(self):
         ids = self.Index.getDocumentsMetadata(metaData='DOCNO')
