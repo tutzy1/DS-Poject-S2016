ba6a8aa5a2365d2f6f7d08edd15fdfa8a06d1b51
tutzy <tutzy1@gmail.com>
2016-09-06 21:44:20 +0300

    few more touchups has been done: 1. back to last page button (with page history maintenance) 2. go to home screen button
    also all the unused functions and section within the user interface file were cleared.
    
    **just need to add the pickle options and we're DONE!!!**

--- Index.py
+++ Index.py
@@ -97,8 +97,8 @@ class IndexEnvironment:
         TextVec = filter(None, TextVec)
         st = PorterStemmer()
         for i in range(len(TextVec)) :
-            term = TextVec[i]
-            stem = st.stem(term)
+            term = TextVec[i].lower()
+            stem = str(st.stem(term))
             Doc.addTerm(term, i)
             Doc.addStem(stem, i)
         self.Update_Index(Doc)
