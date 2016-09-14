6ed8612bfa9a2f7dd9071d81b02f04a439a68e6e
tutzy <tutzy1@gmail.com>
2016-09-14 18:48:22 +0300

    **THE PROJECT IS DONE!!** - some more documentation.

--- UI.py
+++ UI.py
@@ -26,16 +26,31 @@ class Menu(FloatLayout):
     history = ['MainScreen']
 
     def GetLastScreen(self):
+        """
+        :return: The screen that the user visit before the current screen
+        """
         if len(self.history) == 1:
             return 'MainScreen'
         else:
             return self.history.pop()
 
     def Update_Index(self):
+        """
+        This function creates a popup massage contains a file chooser -
+        to choose xml file with documents to load to the index.
+        With press on the "Open" button the method loadIndex_filepopup_callback will be called.
+        :return: nothing.
+        """
         XFileOpen(on_dismiss=self.loadIndex_filepopup_callback, path=expanduser(u'~'),multiselect=False)
         return
 
     def loadIndex_filepopup_callback(self, instance):
+        """
+        This function calls the relevant function to add documents to the index,
+        after the user selected a file.
+        :param instance: contains data about the user's selection.
+        :return: nothing.
+        """
         if instance.is_canceled():
             return
         self.Index.addIndex(instance.selection[0])
@@ -44,10 +59,22 @@ class Menu(FloatLayout):
         XMessage(title='Pressed button: ' + instance.button_pressed, text=s)
 
     def Load_Queries(self):
+        """
+        This function creates a popup massage contains a file chooser -
+        to choose xml file with queries to load to the database.
+        With press on the "Open" button the method loadQuesries_filepopup_callback will be called.
+        :return: nothing.
+        """
         XFileOpen(on_dismiss=self.loadQuesries_filepopup_callback, path=expanduser(u'~'), multiselect=False)
         return
 
     def loadQuesries_filepopup_callback(self, instance):
+        """
+        This function calls the relevant function to add queries to the database,
+        after the user selected a file.
+        :param instance: contains data about the user's selection.
+        :return: nothing.
+        """
         if instance.is_canceled():
             return
         self.Ranker_Environment.loadQueries(instance.selection[0])
@@ -56,25 +83,47 @@ class Menu(FloatLayout):
         XMessage(title='Pressed button: ' + instance.button_pressed, text=s)
 
     def Get_Limit(self):
+        """
+        This function creates a popup massage contains a text input field -
+        to enter limit for the ranking.
+        With press on the "Open" button the method limit_callback will be called.
+        :return: nothing.
+        """
         XTextInput(title='How many documents would you like \nto appear in the result list? \n(insert 0 to unlimit)', text='0', on_dismiss=self.limit_callback)
         return
 
 
     def limit_callback(self, instance):
+        """
+        This function limit value within this class with the value that
+        the user inserted.
+        :param instance: contains data about the user's input.
+        :return: nothing.
+        """
         if not instance.values['text']:
             self.limit = 0
         else:
             self.limit = int(instance.values['text'])
         return
 
-
-
-    def Run_Queries_File(self, limit):
-        self.limit = int(limit)
+    def Run_Queries_File(self):
+        """
+        This function creates a popup massage contains a file chooser -
+        to choose a path to save the results of ranking all the
+        queries in the database.
+        With press on the "Save" button the method _filepopup_Queries_callback will be called.
+        :return: nothing.
+        """
         XFileSave(on_dismiss=self._filepopup_Queries_callback, path=expanduser(u'~'))
         return
 
     def _filepopup_Queries_callback(self, instance):
+        """
+        This function calls the relevant function to run queries from the database
+        and save the results to file, after the user selected a path to save the file.
+        :param instance: contains data about the user's selection.
+        :return: nothing.
+        """
         if instance.is_canceled():
             return
         self.Ranker_Environment.runQueries(pathnameToSave=instance.get_full_name() + ".txt", limit=self.limit)
@@ -82,13 +131,25 @@ class Menu(FloatLayout):
         s += ('\nFilename: %s\nFull path: %s' % (instance.filename, instance.get_full_name()))
         XMessage(title='Pressed button: ' + instance.button_pressed, text=s)
 
-    def Run_Query_File(self, query, limit):
+    def Run_Query_File(self, query):
+        """
+        This function creates a popup massage contains a file chooser -
+        to choose a path to save the results of ranking the query inserted by the user.
+        With press on the "Save" button the method _filepopup_Query_callback will be called.
+        :param query: The query that the user inserted.
+        :return: nothing.
+        """
         self.query1 = query
-        self.limit = int(limit)
         XFileSave(on_dismiss=self._filepopup_Query_callback, path=expanduser(u'~'))
         return
 
     def _filepopup_Query_callback(self, instance):
+        """
+        This function calls the relevant function to run query inserted by the user,
+        and save the results to file, after the user selected a path to save the file.
+        :param instance: contains data about the user's selection.
+        :return: nothing.
+        """
         if instance.is_canceled():
             return
         self.Ranker_Environment.runQuery(query=self.query1, pathnameToSave=instance.get_full_name() + ".txt",limit=self.limit)
@@ -97,6 +158,10 @@ class Menu(FloatLayout):
         XMessage(title='Pressed button: ' + instance.button_pressed, text=s)
 
     def PrintDocsIDs(self):
+        """
+        :return: string contains all the document's IDs in the index, each in
+                a different row.
+        """
         ids = self.Index.getDocumentsMetadata(metaData='DOCNO')
         result = ""
         for id in ids:
@@ -104,6 +169,9 @@ class Menu(FloatLayout):
         return result
 
     def PrintDocs(self):
+        """
+        :return: string contains all the documents in the index.
+        """
         docs = self.Index.getDocumentsMetadata(metaData='TEXT')
         result = ""
         for doc in docs:
@@ -111,6 +179,10 @@ class Menu(FloatLayout):
         return result
 
     def PrintQueriesIDs(self):
+        """
+        :return: string contains all the queries's IDs in the database, each in
+                a different row.
+        """
         ids = self.Ranker_Environment.getQueriesMetadata('number')
         result = ""
         for id in ids:
@@ -118,6 +190,9 @@ class Menu(FloatLayout):
         return result
 
     def PrintQueries(self):
+        """
+        :return: string contains all the queries in the database.
+        """
         queries = self.Ranker_Environment.getQueriesMetadata('text')
         result = ""
         for query in queries:
@@ -125,35 +200,55 @@ class Menu(FloatLayout):
         return result
 
     def Load_Pickle(self):
+        """
+        This function creates a popup massage contains a file chooser -
+        to choose pickle file with index to load.
+        With press on the "Open" button the method loadPickle_filepopup_callback will be called.
+        :return: nothing.
+        """
         XFileOpen(on_dismiss=self.loadPickle_filepopup_callback, path=expanduser(u'~'), multiselect=False)
         return
 
     def loadPickle_filepopup_callback(self, instance):
+        """
+        This function calls the relevant function to load index from pickle file,
+        after the user selected a file.
+        :param instance: contains data about the user's selection.
+        :return: nothing.
+        """
         if instance.is_canceled():
             return
         f = open(instance.selection[0], 'rb')
         self.Index = pickle.load(f)
         f.close()
+        self.Ranker_Environment = RankerEnvironment(self.Index)
         s = 'The Pickle was uploaded'
         s += ('\nSelection: %s' % instance.selection[0])
         XMessage(title='Pressed button: ' + instance.button_pressed, text=s)
 
     def Save_to_pickle(self):
+        """
+        This function creates a popup massage contains a file chooser -
+        to choose a path to save the index to a pickle file within.
+        With press on the "Save" button the method _filepopup_SavePickle_callback will be called.
+        :return: nothing.
+        """
         XFileSave(on_dismiss=self._filepopup_SavePickle_callback, path=expanduser(u'~'))
         return
 
     def _filepopup_SavePickle_callback(self, instance):
+        """
+        This function calls the relevant function to save index to a pickle file,
+        after the user selected the path.
+        :param instance: contains data about the user's selection.
+        :return: nothing.
+        """
         if instance.is_canceled():
             return
         self.Index.Put_Index_In_Pickle(pathname=instance.get_full_name())
         s = 'The index was saved to pickle!'
         s += ('\nFilename: %s\nFull path: %s' % (instance.filename, instance.get_full_name()))
         XMessage(title='Pressed button: ' + instance.button_pressed, text=s)
-
-
-    def is_dir(self, directory, filename):
-        return isdir(join(directory, filename))
-
 
 class MenuApp(App):
     def build(self):
